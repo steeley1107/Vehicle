c35aae4dc2ad8fcd9c9f90b8b44b0a86c9360781
steeley1107 <steeley1107@gmail.com>
2015-10-24 12:28:56 -0700

    overwrite methods in car class

--- Vehicles/Car.m
+++ Vehicles/Car.m
@@ -19,4 +19,31 @@
     return self;
 }
 
+#pragma mark - Private method implementations
+- (NSString *)start
+{
+    return [NSString stringWithFormat:@"Start power source %@.", self.powerSource];
+}
+
+#pragma mark - Superclass Overrides
+- (NSString *)goForward
+{
+    return [NSString stringWithFormat:@"%@ %@ Then depress gas pedal.", [self start], [self changeGears:@"Forward"]];
+}
+
+- (NSString *)goBackward
+{
+    return [NSString stringWithFormat:@"%@ %@ Check your rear view mirror. Then depress gas pedal.", [self start], [self changeGears:@"Reverse"]];
+}
+
+- (NSString *)stopMoving
+{
+    return [NSString stringWithFormat:@"Depress brake pedal. %@", [self changeGears:@"Park"]];
+}
+
+- (NSString *)makeNoise
+{
+    return @"Beep beep!";
+}
+
 @end
